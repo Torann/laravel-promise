dd47be3bc0a9c212f354ec10d69bf00132ef972d
Daniel Stainback <torann@gmail.com>
2014-09-04 12:40:37 -0400

    Add the option to ignore super admin setting when checking user permission or role

--- src/Torann/Promise/HasRole.php
+++ src/Torann/Promise/HasRole.php
@@ -50,17 +50,18 @@ trait HasRole
     /**
      * Determine if a user has a given role.
      *
-     * @param name The name of the role.
+     * @param  string  $name              The name of the role.
+     * @param  bool    $ignoreSuperAdmin  Allows for ignoring the super admin setting.
      * @return bool
      */
-    public function hasRole( $name )
+    public function hasRole( $name, $ignoreSuperAdmin = false )
     {
         $roles = explode(',', strtolower($name) );
 
         foreach ($this->roles as $role)
         {
             // Return true for super admin
-            if ($role->name === Config::get('promise::super_admin')) {
+            if ($ignoreSuperAdmin === false && $role->name === Config::get('promise::super_admin')) {
                 return true;
             }
 
@@ -76,17 +77,18 @@ trait HasRole
     /**
      * Check if user has a permission by its name
      *
-     * @param string $permission Permission string.
+     * @param  string  $permission        Permission string.
+     * @param  bool    $ignoreSuperAdmin  Allows for ignoring the super admin setting.
      * @return boolean
      */
-    public function can( $name )
+    public function can( $name, $ignoreSuperAdmin = false )
     {
         $names = explode(',', strtolower($name) );
 
         foreach ($this->roles as $role)
         {
             // Return true for super admin
-            if ($role->name === Config::get('promise::super_admin')) {
+            if ($ignoreSuperAdmin === false && $role->name === Config::get('promise::super_admin')) {
                 return true;
             }
 
